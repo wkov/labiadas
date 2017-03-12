04f7c79090bd7056591b1925583baa23c6e5435f
wkov <borruey.sergi@gmail.com>
2017-03-12 10:23:02 +0100

    static configuration in settings

--- labiadas/settings.py
+++ labiadas/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/romani/static/'
 # STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static').replace('\\','/'),]
 
 
