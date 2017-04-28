0f90378454fae71f17173bceb78b00e0176ab0b2
wkov <borruey.sergi@gmail.com>
2017-04-28 02:30:18 +0200

    gestio pels productors

--- romani/views.py
+++ romani/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-import simplejson as simplejson
 from .models import Producte, Productor, Comanda, Contracte, TipusProducte, Node, DiaEntrega, FranjaHoraria, Key, Convidat, Frequencia
 from django.db.models import Q
 from django.contrib.auth import get_user_model
