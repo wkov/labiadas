97f728d8e901caff44e9ef183dae1d0634d9c7f0
wkov <borruey.sergi@gmail.com>
2017-05-16 12:12:23 +0200

    gestio pels productors

--- romani/models.py
+++ romani/models.py
@@ -36,7 +36,7 @@ class Adjunt(models.Model):
 
     def validate_file(fieldfile_obj):
         filesize = fieldfile_obj.file.size
-        megabyte_limit = 5.0
+        megabyte_limit = 2.0
         if filesize > megabyte_limit*1024*1024:
             raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
 
