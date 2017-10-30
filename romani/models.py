00ba3a5966dc45e658eb36a90cb6d86f8d455ccd
wkov <borruey.sergi@gmail.com>
2017-10-31 00:25:35 +0100

    retocs

--- romani/models.py
+++ romani/models.py
@@ -162,7 +162,7 @@ class Producte(models.Model):
     text_curt = models.TextField(blank=False, max_length=75)
     descripcio = models.TextField(blank=True, default="")
     datahora = models.DateTimeField(auto_now_add=True)
-    foto = models.FileField(upload_to='documents/%Y/%m/%d', null=True, validators=[validate_file])
+    foto = models.FileField(upload_to='productes/%Y/%m/%d', null=True, validators=[validate_file])
     productor = models.ForeignKey(Productor)
     keywords = models.TextField(blank=True, verbose_name='Paraules Clau')
     frequencies = models.ForeignKey(Frequencia)
