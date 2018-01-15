f2eacbd0c488e35f7aea3b721d2eec2196ec60e8
wkov <borruey.sergi@gmail.com>
2018-01-15 01:45:27 +0100

    imatges de productes 250x250px  limit

--- romani/serializers.py
+++ romani/serializers.py
@@ -13,26 +13,3 @@ class ProducteSerializer(serializers.ModelSerializer):
         depth = 1
         fields = ('pk', 'nom', 'etiqueta', 'foto', 'productor', 'thumb', 'text_curt', 'formats')
 
-
-class FormatSerializer(serializers.ModelSerializer):
-
-    # formats_nom = serializers.RelatedField(source='formats', read_only=True)
-    # formats = serializers.StringRelatedField(many=True)
-
-
-    class Meta:
-        model = TipusProducte
-        depth = 1
-        fields = ('pk')
-
- #
- # nom = models.CharField(max_length=20)
- #    etiqueta = models.ForeignKey(Etiqueta)
- #    text_curt = models.TextField(blank=False, max_length=75)
- #    descripcio = models.TextField(blank=True, default="")
- #    datahora = models.DateTimeField(auto_now_add=True)
- #    foto = models.FileField(upload_to='productes/%Y/%m/%d', null=True, validators=[validate_file])
- #    thumb = models.FileField(blank=True, null=True)
- #    productor = models.ForeignKey(Productor)
- #    keywords = models.TextField(blank=True, verbose_name='Paraules Clau')
- #    frequencies = models.ForeignKey(Frequencia)
