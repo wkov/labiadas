4e3e103702ca085fe34db97a702efb8d0d1af5fd
wkov <borruey.sergi@gmail.com>
2018-01-19 13:20:23 +0100

    allow refresh oj JWTauth

--- labiadas/settings.py
+++ labiadas/settings.py
@@ -178,12 +178,13 @@ REST_FRAMEWORK = {
     ),
 }
 
-
+import datetime
 
 JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER':
         'romani.api.jwt_payload_handler',
     'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=150)
     # 'JWT_AUTH_HEADER_PREFIX': '',
     # 'JWT_PAYLOAD_GET_USER_ID_HANDLER':
     # 'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
