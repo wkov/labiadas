2f2f4cbf1e6a874681b699d07ad983d8a04d4a12
wkov <borruey.sergi@gmail.com>
2018-07-27 21:59:11 +0200

    correcions v2

--- romani/pers_views.py
+++ romani/pers_views.py
@@ -1,9 +1,10 @@
 
 from romani.models import UserProfile
 from django.shortcuts import render
+from django.contrib.auth.models import  User
 
 def UserDetailView(request, pk):
 
-    user = User.objects.get(username=username)
+    user = User.objects.get(pk=pk)
     userp = UserProfile.objects.get(user=user)	
     return render(request, "romani/consumidors/perfil.html", {'userp': userp})
