0cbc907afc874d34d77bf80210e261ef5bf589c7
wkov <borruey.sergi@gmail.com>
2018-07-27 15:50:04 +0200

    correccio

--- romani/pers_views.py
+++ romani/pers_views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def UserDetailView(request, pk):
 
-    userp = UserProfile.objects.get(pk=pk)
-
+    user = User.objects.get(pk=pk)
+    userp = UserProfile.objects.get(user=user)	
     return render(request, "romani/consumidors/perfil.html", {'userp': userp})
